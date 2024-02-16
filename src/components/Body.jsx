cf73162f5890a2944cb1617a303eaf85e5b2e79a
Ozyugoo <stellaozy22@gmail.com>
2024-02-16 02:55:41 +0100

    Feat: Add content to body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,29 @@
 function Body() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="self-intro">
+        <h1 className="self">
+          HELLO, I AM STELLA OZIOMA UGOO-OKONKWO&middot;
+        </h1>
+
+        <p className="description">
+          An ardent front-end developer specialized in building responsive website with regards to accessibility. I profer solutions to software development issues and I am passionate about creating a green digital product.
+        </p>
+
+        <button>
+          Download Resume &nbsp; <em>&#x2022;</em>
+        </button>
+      </div>
+
+      <div>
+        
+      </div>
+      <hr />
+
+      <div className="self-intro">
+        <h2>FEATURED PROJECTS</h2>
+      </div>
+    </>
   )
 }
 
