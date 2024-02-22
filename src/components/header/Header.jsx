154c070a7f5839fb0eab836a51e0e142d10ca75e
Ozyugoo <stellaozy22@gmail.com>
2024-02-22 02:12:40 +0100

    Chore: Updated and modified header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,27 @@
 import "./header.css"
+import Hero from "../hero/Heros.jsx";
 
 function Header() {
   return (
     <>
-      <div className="header-container">
-        <p className="name">Stella Ozioma Ugoo-Okonkwo</p>
+      <div className="heading">
+        <div className="header-container">
+          <p className="name">Stella</p>
+          <p className="name2" hidden>Stella Ugoo-Okonkwo</p>
+        </div>
+        <div className="nav-bar">
+          <nav>
+              <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Contact</a></li>
+              </ul>
+          </nav>
+        </div>
       </div>
-      <div className="nav-bar">
-        <nav>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </nav>
+
+      <div>
+        <Hero />
       </div>
     </>
   )
