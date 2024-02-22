3f36da0560b1ef82dd94e5110a1806f202454e20
Ozyugoo <stellaozy22@gmail.com>
2024-02-22 02:12:05 +0100

    Chore: Modified body component and move the existing content to header component

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,33 +1,11 @@
-import "./body.css"
-
+import "./body.css";
+ 
 function Body() {
   return (
-    <>
-      <div className="self-intro">
-        <h1 className="self">
-          HELLO, I AM STELLA OZIOMA UGOO-OKONKWO&middot;
-        </h1>
-
-        <p className="description">
-          An ardent front-end developer specialized in building responsive website with regards to accessibility. I profer solutions to software development issues and I am passionate about creating a green digital product.
-        </p>
-
-        <button>
-          Download Resume &nbsp; <em>&#x2022;</em>
-        </button>
-      </div>
-
+    <> 
       <div>
-        
-      </div>
-      <hr />
-
-      <div className="self-intro">
-        <h2>FEATURED PROJECTS</h2>
-        <p>
-          Here are some of the projects I worked on that showcased my skills and passion for front-end development.
-        </p>
-      </div>
+         
+      </div>      
     </>
   )
 }
