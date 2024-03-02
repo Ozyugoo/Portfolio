2c08e5871d74a0d8667222decd59ab7210961ccc
Ozyugoo <stellaozy22@gmail.com>
2024-03-02 02:41:52 +0100

    Chore: modify header file

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,17 +5,17 @@ import '../body/body.css'
 function Header() {
   return (
     <>
-      <div className="heading .col12">
-        <div className="header-container .col6">
+      <div className="heading col12">
+        <div className="header-container col6">
           <p className="name">Stella</p>
           <p className="name2" hidden>Stella Ugoo-Okonkwo</p>
         </div>
-        <div className="nav-bar .col4">
-          <nav>
-              <ul className=".col12">
-                  <li className=".col1"><a href="#">Home</a></li>
-                  <li className=".col1"><a href="#">About</a></li>
-                  <li className=".col1"><a href="#">Contact</a></li>
+        <div className="nav-bar col6">
+          <nav className="col12">
+              <ul className="col12">
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Contact</a></li>
               </ul>
           </nav>
         </div>
