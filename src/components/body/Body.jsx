38a9c5b3661bc5037edf6aede37a14539480f64a
Ozyugoo <stellaozy22@gmail.com>
2024-03-02 02:39:41 +0100

    Update: make changes to body,
    Chore: Move data to other folder

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,11 +1,16 @@
 import "./body.css";
+import Project from "./Project";
  
 function Body() {
   return (
     <> 
       <div>
-         
-      </div>      
+         <h2>Featured Projects</h2>
+        <p>Here are some of the selected projects that showcase my work and passion for front-end development.</p>
+      </div>
+      <div>
+        <Project />
+      </div>
     </>
   )
 }
