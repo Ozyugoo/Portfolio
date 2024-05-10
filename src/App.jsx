09c9991bcba1659bd58f9a80515208b0482d28a7
Ozyugoo <stellaozy22@gmail.com>
2024-05-10 15:13:15 +0100

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Project from "./pages/Projects.jsx";
+import NotFound from "./pages/NotFound.jsx";
 import "../src/index.css";
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="project" element={<Project />} />
-          <Route path="*" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </div>
