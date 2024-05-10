e4326bdc2735b751307d51551657880ef13adae6
Ozyugoo <stellaozy22@gmail.com>
2024-05-10 15:08:52 +0100

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,22 @@
-import {
-  createBrowserRouter,
-  BrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { createRoot } from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 import Project from "./pages/Projects.jsx";
 import "../src/index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "",
-    element: <Home />,
-  },
-
-  {
-    path: "Home",
-    element: <Home />,
-  },
-  {
-    path: "Project",
-    element: <Project />,
-  },
-  {
-    path: "About",
-    element: <About />,
-  },
-]);
-
 function App() {
   return (
-    <div className="container">
-      <section>
-        <RouterProvider router={router} />
-      </section>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="project" element={<Project />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
-const root = createRoot(document.getElementById("root"));
-
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
 
 export default App;
